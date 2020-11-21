84d994cabc76efff6f7519341e315664aacf39c3
Hrishikesh Thesserikaran <hrishikeshts@gmail.com>
2020-11-21 18:35:43 +0530

    Added DownloadApp component

--- src/components/DownloadApp.js
+++ src/components/DownloadApp.js
@@ -3,7 +3,9 @@ import React from "react";
 const DownloadApp = () => {
     return (
         <>
-            <h3>Download App</h3>
+            <h3>Download App</h3>Lorem ipsum dolor sit amet, consectetur
+            adipiscing elit. A vulputate elit fringilla consectetur quis lorem
+            augue lobortis. Suscipit. <aside id='phones'></aside>
         </>
     );
 };
--- src/components/ourteam.js
+++ src/components/ourteam.js
@@ -5,8 +5,8 @@ import { members } from "./members";
 export default function Ourteam() {
     return (
         <div>
-            <h3>Our Team</h3>
-            <section>
+            <section id='ourteam'>
+                <h3>Our Team</h3>
                 {members.map((member) => {
                     return <Member key={member.id} {...member} />;
                 })}
