8f2498994a0d05cf9d74dc18778f6769463c3446
Hrishikesh Thesserikaran <hrishikeshts@gmail.com>
2020-12-01 10:33:04 +0530

    Add logo and text on banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import Banner from "../graphics/Child-With-Arms-in-the-Air.jpg";
+import whiteLogo from "../graphics/airli-logo-white.png";
 
 export default function Home() {
     const [scroll, setScroll] = useState(false);
@@ -12,11 +13,23 @@ export default function Home() {
     window.addEventListener("scroll", changeBrightness);
 
     return (
-        <img
-            src={Banner}
-            alt='Airli'
-            className={scroll ? `banner-scroll` : `banner`}
-            draggable='false'
-        />
+        <div id='banner'>
+            <img
+                src={Banner}
+                alt='Airli'
+                className={scroll ? `banner-scroll` : `banner`}
+                draggable='false'
+            />
+            <img
+                src={whiteLogo}
+                alt='Airli'
+                className='whitelogo'
+                draggable='false'
+            />
+            <div className={scroll ? `text-scroll` : `text`}>
+                Creating Better Quality of Life for Asthma sufferers. <br />
+                Everyday. Everywhere. Everyone.
+            </div>
+        </div>
     );
 }
