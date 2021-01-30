ffd0b5ca87176dd5a4a2638c07b07eaacd02ab57
Hrishikesh Thesserikaran <hrishikeshts@gmail.com>
2021-01-31 02:27:31 +0530

    Add BREATHE EASY to Other

--- src/components/businessplan.js
+++ src/components/businessplan.js
@@ -5,64 +5,200 @@ import Values from "./values";
 import Strategy from "./strategy";
 
 export default function Businessplan() {
-  return (
-    <div id="other">
-      <div className="container">
-        <h3>Business Plan</h3>
-        <p class="text-justify">
-          The primary business for Airli is those suffering a Chronic
-          Respiratory Condition, like asthma data state level. Currently, there
-          is 11.2% of the Australian population suffering from this chronic
-          condition, with the New South Wales’ proportion approximated at 10.7%
-          (Statistics n.d). With a growing population, there is a steady
-          increase in observed Australians with asthma. Upon launch, airli’s
-          software application (app), will provide consumers with analyzed
-          pollen count data that can be accurately forecasted for up to 1 week.
-        </p>
-        <p class="text-justify">
-          Currently, airli is among an emerging industry of vast competitors
-          that have huge potential for market growth. This growth will reflect
-          on how well airli adapt to their current digital service offerings, to
-          which they can benefit from learning from other technological and
-          health services, and of course the consumer’s acceptance of these
-          products into their everyday lives. The greatest direct national
-          competitor airli faces in the industry is the Government owned
-          organization, Aus pollen.
-        </p>
-        <p class="text-justify">
-          With respect to the budget of $50 000, strategies are to be determined
-          for the critical issues: lack of funding, lack of cohesion in
-          marketing communications and product strategy and industry awareness.
-          Using the 7 P’s of marketing in a unique perspective, can give airli a
-          competitive advantage. Starting with phase 1, options for sources of
-          funds include venture capital, government grants and equity crowd
-          funding. Phase 2 involves post launch and prelaunch of the
-          application, and strategies to match. Prelaunch strategies include,
-          establishing a cohesive social media presence, including rebranding of
-          the logo, website, Google My Business profile, Google Ad Words and
-          Facebook profile. Post launch strategies will involve relevant
-          tradeshows and conferences, as well as the continuation of social
-          media throughout the operation of Airli.
-        </p>
-        <p class="text-justify">
-          Within the 12 months, provision of funding should be available to
-          commence the marketing plan. Securing the capital will have a
-          significant impact on your ability to execute this plan and to make
-          key improvements in areas such as web development, your actualized
-          service and social media engagement. Post Phase 1, it is recommended
-          that airli secure a sizeable customer database to explore potential
-          B2B revenue streams as part of a 3 year plan.
-        </p>
-      </div>
-      <div className="container div-down"></div>
-      <OurStory />
-      <div className="container div-up"></div>
-      <MissionVision />
-      <div className="container div-down"></div>
-      <Values />
-      <div className="container div-up"></div>
-      <Strategy />
-      <div className="container div-down"></div>
-    </div>
-  );
+    return (
+        <div id='other'>
+            <div className='container'>
+                <h3>Business Plan</h3>
+                <p class='text-justify'>
+                    The primary business for Airli is those suffering a Chronic
+                    Respiratory Condition, like asthma data state level.
+                    Currently, there is 11.2% of the Australian population
+                    suffering from this chronic condition, with the New South
+                    Wales’ proportion approximated at 10.7% (Statistics n.d).
+                    With a growing population, there is a steady increase in
+                    observed Australians with asthma. Upon launch, airli’s
+                    software application (app), will provide consumers with
+                    analyzed pollen count data that can be accurately forecasted
+                    for up to 1 week.
+                </p>
+                <p class='text-justify'>
+                    Currently, airli is among an emerging industry of vast
+                    competitors that have huge potential for market growth. This
+                    growth will reflect on how well airli adapt to their current
+                    digital service offerings, to which they can benefit from
+                    learning from other technological and health services, and
+                    of course the consumer’s acceptance of these products into
+                    their everyday lives. The greatest direct national
+                    competitor airli faces in the industry is the Government
+                    owned organization, Aus pollen.
+                </p>
+                <p class='text-justify'>
+                    With respect to the budget of $50 000, strategies are to be
+                    determined for the critical issues: lack of funding, lack of
+                    cohesion in marketing communications and product strategy
+                    and industry awareness. Using the 7 P’s of marketing in a
+                    unique perspective, can give airli a competitive advantage.
+                    Starting with phase 1, options for sources of funds include
+                    venture capital, government grants and equity crowd funding.
+                    Phase 2 involves post launch and prelaunch of the
+                    application, and strategies to match. Prelaunch strategies
+                    include, establishing a cohesive social media presence,
+                    including rebranding of the logo, website, Google My
+                    Business profile, Google Ad Words and Facebook profile. Post
+                    launch strategies will involve relevant tradeshows and
+                    conferences, as well as the continuation of social media
+                    throughout the operation of Airli.
+                </p>
+                <p class='text-justify'>
+                    Within the 12 months, provision of funding should be
+                    available to commence the marketing plan. Securing the
+                    capital will have a significant impact on your ability to
+                    execute this plan and to make key improvements in areas such
+                    as web development, your actualized service and social media
+                    engagement. Post Phase 1, it is recommended that airli
+                    secure a sizeable customer database to explore potential B2B
+                    revenue streams as part of a 3 year plan.
+                </p>
+            </div>
+            <div className='container div-up'></div>
+            <OurStory />
+            <div className='container div-down'></div>
+            <MissionVision />
+            <div className='container div-up'></div>
+            <Values />
+            <div className='container div-down'></div>
+            <Strategy />
+            <div className='container div-up'></div>
+            <div className='container'>
+                <h3>BREATHE EASY</h3>
+                <h4 className='blue-text'>by making the Invisible Visible</h4>
+                <p class='text-justify'>
+                    Airli BREATHE EASY, by making the Invisible Visible Airli is
+                    designed to offer patient centric alerts reducing incidents
+                    of asthma and sudden burst of the need for acute are and
+                    emergency hospital admissions. It will have an analytics
+                    platform to analyze data from multiple data sources running
+                    AI and machine learning algorithms and is built on
+                    technology to provide complete data security and privacy
+                </p>
+                <p class='text-justify'>
+                    The airli product development is based on leveraging
+                    Research Engineering core competencies in creating this
+                    unique solution. The research and early stage design
+                    thinking is endorsed in principle and supported by CSIRO and
+                    some renowned academic and research professionals worldwide.
+                    The product presents strong encouragement to further develop
+                    the product and roll out in support of the healthcare sector
+                    via a strategic roadmap. The further analytics development
+                    face will lead to predicting causes and situations that lead
+                    to asthma and its management
+                </p>
+                <p class='text-justify'>
+                    As many as 300 million people suffer from asthma in the
+                    World and will reach 400 million soon. In Australia, the
+                    overall financial cost of asthma is estimated to be $30
+                    billion and also the cost to patients due to the reduced
+                    quality of life is around $ 21.5 billion. According to World
+                    Bank estimates, it costs the World’s economies $5 trillion
+                    per year
+                </p>
+                <p class='text-justify'>
+                    Airli is an Australian innovation established to assist
+                    consumers “Breathe Easy by making the Invisible Visible” by
+                    providing real time contextualized and relevant location
+                    based air quality health data
+                </p>
+                <p class='text-justify'>
+                    We have embraced AI & Machine Learning predictive algorithms
+                    to triangulate geospatial, weather and location based
+                    consumer information. It is an “API first” digital platform
+                    business embracing B2C, B2B and B2B2C business models. Phase
+                    one of the initial roll out will focus on predictive pollen
+                    count analytics for asthma sufferers
+                </p>
+                <p class='text-justify'>
+                    The founders bring together deep and diverse domain
+                    experience anchored in geospatial research and technology to
+                    solve consumer related health problems through supporting
+                    behavioral change and digital analytics
+                </p>
+                <p class='text-justify'>
+                    Airli received a Minimum Viable Product (MVP) grant from the
+                    NSW Government to help develop its technology. Govt and
+                    CSIRO grants are in the pipeline for further funding of the
+                    entire project. Airli is working with a large not-for-profit
+                    Asthma Organization that intends to put the launch of the
+                    commercial product in its calendar
+                </p>
+                <div className='orange-text'>
+                    “Airli brings together deep and diverse domain expertise
+                    anchored in geospatial research and technology to solve
+                    consumer related health problems through supporting
+                    behavioral change and analytics. We are seeking partners to
+                    further develop and progress our AIRLI product and services”
+                </div>
+                <p class='text-justify'>
+                    Airli will incorporate an advanced analytics platform to
+                    analyze data from multiple data sources: Satellites, UAV’s,
+                    ground systems IoT and many more ubiquitous devises and
+                    social media networks. It will use AI, ML algorithms to
+                    analyze spatial and time variant data, in order to process
+                    and predict the disease triggers including proportion of
+                    pollutants, movement of air-borne particles and other
+                    related data to create targeted solutions for the health
+                    risk. The information will be used to push alerts and
+                    notifications to the patients or carer
+                </p>
+                <p class='text-justify'>
+                    “Airli Unique strengths differentiators and rationale”
+                </p>
+                <p class='text-justify'>
+                    Airli is designed with an objective to disrupt, with a
+                    paradigm shift from delayed interventional to predictive and
+                    personal, from reactive to preventive and from disease to
+                    wellness.
+                </p>
+                <p class='text-justify'>
+                    Airli will proactively provide some relevant highly useful
+                    and vital alerts to asthma sufferers. These alerts may also
+                    be useful to the healthcare sector stake holders including
+                    the GP’s, specialist clinicians and Hospitals. These alerts
+                    may also be of some salient value for the optimization of
+                    their supply chain, particularly in case of likely acute
+                    alerts for the events like the thunderstorm asthma
+                </p>
+                <h3>Why Airli?</h3>
+                <div>
+                    <ul className='text-left check'>
+                        <li className='text-left'>
+                            Compliant with the PPPM (Predictive Preventive and
+                            Personalized) approach
+                        </li>
+                        <li className='text-left'>
+                            Compliant with value based and a patient centric
+                            approach
+                        </li>
+                        <li className='text-left'>
+                            Backed up by well - rounded expertise in space,
+                            biology, AI / ML, advanced data analytics and a
+                            conglomeration of other technology assets
+                        </li>
+                        <li className='text-left'>
+                            Airli is designed to provide 24x7x365 real-time data
+                            with pixel level accuracy
+                        </li>
+                        <li className='text-left'>
+                            Being a satellite based system airli can be easily
+                            scaled upon a global scale
+                        </li>
+                        <li className='text-left'>
+                            Airli use AI/ML algorithms to analyze spatial data,
+                            to process and predict disease triggers
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div className='container div-down'></div>
+        </div>
+    );
 }
